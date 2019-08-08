5f025ddbc3f24a602b6a9a2c48499747b939496f
Jérémy Levilain <jeremylevilain78@orange.fr>
2019-08-09 01:06:34 +0200

    feat: add final recipes for the actual content
    * feat: rework Magnetic Card and RFID Card recipes
    
    * feat: use wildcard in recipes, add missing recipes

--- tools/generate_card_recipes.sh
+++ tools/generate_card_recipes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROOT="../src/main/resources/assets/slimeperipherals"
+OUTPUT="$ROOT/recipes/generated"
 
 colors=(white orange magenta lightblue yellow lime pink gray silver cyan purple blue brown green red black)
 data_src=(15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0)
@@ -8,15 +9,15 @@ data_dest=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
 
 len=${#colors[@]}
 
-for (( i=0; i<$len; i++ )); do
+for (( i=1; i<$len; i++ )); do
     name="magnetic_card_${colors[$i]}"
-	cat > $ROOT/recipes/$name.json <<EOF
+	cat > $OUTPUT/$name.json <<EOF
 {
   "type": "minecraft:crafting_shapeless",
   "ingredients": [
     {
       "item": "slimeperipherals:magnetic_card",
-      "data": 0
+      "data": 32767
     },
     {
       "item": "minecraft:dye",
@@ -36,15 +37,15 @@ EOF
     echo "Generated recipe $name."
 done
 
-for (( i=0; i<$len; i++ )); do
+for (( i=1; i<$len; i++ )); do
     name="rfid_card_${colors[$i]}"
-	cat > $ROOT/recipes/$name.json <<EOF
+	cat > $OUTPUT/$name.json <<EOF
 {
   "type": "minecraft:crafting_shapeless",
   "ingredients": [
     {
       "item": "slimeperipherals:rfid_card",
-      "data": 0
+      "data": 32767
     },
     {
       "item": "minecraft:dye",
